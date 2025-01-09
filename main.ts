49d5f7bf435c71d312cff95b8c2ba8089906e93c
Elliott-Roach <elliott.roach@mths.ca>
2025-01-09 11:03:37 -0500

    forward and left compleat

--- main.ts
+++ main.ts
@@ -7,24 +7,43 @@
 
 //setup
 let forward: number = 0
+let left: number = 0
 basic.showIcon(IconNames.Happy)
 
 //moving forward
 if (forward == 0) {
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
     forward = 1
-    while(forward = 1){
+    while(forward == 1){
         robotbit.StpCarMove(1, 51)
+        basic.pause(10)
+        //stopping
+        if (forward == 1){
+            input.onButtonPressed(Button.AB, function () {
+                basic.clearScreen()
+                forward = 0
+        })
+        }
     }
 })
 }
 
-//stopping
-if (forward == 1){
+//turning left
+if (left == 0) {
     input.onButtonPressed(Button.A, function () {
         basic.clearScreen()
-        forward = 0
-})
+        left = 1
+        while (left == 1) {
+            robotbit.StpCarTurn(10, 51, 125)
+            basic.pause(10)
+            //stopping
+            if (left == 1) {
+                input.onButtonPressed(Button.A, function () {
+                    basic.clearScreen()
+                    left = 0
+                })
+            }
+        }
+    })
 }
-    
